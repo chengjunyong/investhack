24ab96614c437073861e7efd148edfb1396058f5
Ethan <junyong1213@hotmail.com>
2020-11-17 23:02:35 +0800

    Initialize

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<?php include 'header.php'?>
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/lightweight-charts/dist/lightweight-charts.standalone.production.js"></script>
+<script>
+	const chart = LightweightCharts.createChart(document.body, { width: 400, height: 300 });
+	const lineSeries = chart.addLineSeries();
+	lineSeries.setData([
+	    { time: '2019-04-11', value: 80.01 },
+	    { time: '2019-04-12', value: 96.63 },
+	    { time: '2019-04-13', value: 76.64 },
+	    { time: '2019-04-14', value: 81.89 },
+	    { time: '2019-04-15', value: 74.43 },
+	    { time: '2019-04-16', value: 80.01 },
+	    { time: '2019-04-17', value: 96.63 },
+	    { time: '2019-04-18', value: 76.64 },
+	    { time: '2019-04-19', value: 81.89 },
+	    { time: '2019-04-20', value: 74.43 },
+	]);
+</script>
+
+<?php include 'footer.php'?>

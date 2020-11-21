bdbabb3c4b6903e7365d2a0cb6085081f81df7de
cheeyon97 <cheeyon97@hotmail.com>
2020-11-21 20:23:01 +0800

    missing cy part db

--- script/dbcon.php
+++ script/dbcon.php
@@ -48,4 +48,93 @@ function getMachineById($conn,$id){
 	return $result;
 }
 
+function getMentor($conn){
+	$sql = "SELECT * FROM guru ORDER BY roi DESC	";
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+function getOpinion($conn){
+	$sql = "SELECT * FROM guru_post";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+
+function getGuruById($conn,$id){
+	$sql = "SELECT * FROM guru WHERE id=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+
+function getUserById($conn,$id){
+	$sql = "SELECT * FROM user WHERE id=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+function getPostById($conn,$id){
+	$sql = "SELECT * FROM guru_post WHERE guruid=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+function getStockById($conn,$id){
+	$sql = "SELECT * FROM guru_portfolio WHERE guruid=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+function getEducationById($conn,$id){
+	$sql = "SELECT * FROM guru_education WHERE guruid=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+
+function getForumTopicById($conn,$id){
+	$sql = "SELECT * FROM forum_topic WHERE guruid=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+function getForumCommentById($conn,$id){
+	$sql = "SELECT * FROM forum_comment WHERE topicid=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+function getMentorById($conn,$id){
+	$sql = "SELECT * FROM mentor WHERE userid=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+function getChatById($conn,$id){
+	$sql = "SELECT * FROM chat WHERE guruid=".$id ;
+	$result = $conn->query($sql);
+
+	return $result;
+}
+
+function getLatestChatById($conn,$userid){
+	$sql = "SELECT * FROM chat ORDER BY id DESC LIMIT 1";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+function getLatestGuruChatById($conn,$guruid){
+	$sql = "SELECT * FROM chat WHERE guruid=".$guruid." ORDER BY id DESC LIMIT 1";
+	$result = $conn->query($sql);
+	return $result;
+}
+
 ?>
